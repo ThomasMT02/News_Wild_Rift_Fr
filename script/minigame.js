6907578e2a2718a27c4c84ffc683557e39306fbe
PapiManuMT <emmanuel.pricot25@gmail.com>
2023-07-14 10:15:08 +0200

    transfert du script

---
+++ script/minigame.js
@@ -0,0 +1,14 @@
+console.log("MT_Script 🦊");
+
+const response = document.querySelectorAll(".response");
+let button = document.querySelectorAll(".buttun");
+
+// console.log(response);
+
+for (i=1; i<button.length; i++) {
+    if (button.length = i) {
+        button.classList.add("button"+i)
+    }
+}
+
+console.log(button);
